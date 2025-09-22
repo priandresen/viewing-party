57d7f13f646ab1c9867586dd979804d536ef71c6
Helen <lelinewborncare@gmail.com>
2025-09-22 12:00:16 -0700

    Implemented add to watch fuction

--- viewing_party/party.py
+++ viewing_party/party.py
@@ -18,7 +18,13 @@ def add_to_watchlist(user_data, movie):
     user_data["watchlist"].append(movie)
     return user_data
 
+def watch_movie(user_data, title):
+    for movie in user_data["watchlist"]:
+        if movie["title"] == title:
+            user_data["watchlist"].remove(movie)
+            user_data["watched"].append(movie)
 
+    return user_data
 
 # -----------------------------------------
 # ------------- WAVE 2 --------------------
