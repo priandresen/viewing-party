05e0c1d933004f321cdf3b30d54783347fe5eaa2
Helen <lelinewborncare@gmail.com>
2025-09-22 11:36:44 -0700

    Implemented add to watched fuction

--- viewing_party/party.py
+++ viewing_party/party.py
@@ -10,6 +10,9 @@ def create_movie(title, genre, rating):
         "rating": rating
     }
 
+def add_to_watched(user_data, movie):
+    user_data["watched"].append(movie)
+    return user_data
 
 
 # -----------------------------------------
