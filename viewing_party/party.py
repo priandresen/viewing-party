428a60de3cec6b3a46a405a16ec50ee262d7739c
Priscilla Andresen <pgdamorim@gmail.com>
2025-09-22 13:46:25 -0700

    adding wave 2 comments to visualize the problem

--- viewing_party/party.py
+++ viewing_party/party.py
@@ -40,6 +40,30 @@ def get_watched_avg_rating(user_data):
 
     return avg_rating / len(user_data["watched"])
 
+
+"""Create a function named `get_most_watched_genre`. This function should...
+
+- take one parameter: `user_data`
+  - the value of `user_data` will be a dictionary with a `"watched"` list of movie dictionaries. Each movie dictionary has a key `"genre"`.
+    - This represents that the user has a list of watched movies. Each watched movie has a genre.
+    - The values of `"genre"` is a string.
+- Determine which genre is most frequently occurring in the watched list
+- return the genre that is the most frequently watched
+- If the value of "watched" is an empty list, `get_most_watched_genre` should return `None`.
+"""
+
+def get_most_watched_genre(user_data):
+
+    most_watched_genre = None
+    
+
+    for movie in user_data["watched"]:
+
+
+
+
+    return most_watched_genre
+
     
 # -----------------------------------------
 # ------------- WAVE 3 --------------------
